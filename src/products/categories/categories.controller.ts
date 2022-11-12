28e5890173706145024ab9c6448f675f04ff2c87
Feride Elhennawy <40718869+FerideElhennawy@users.noreply.github.com>
2022-11-12 10:48:01 +0300

    Activate category service

--- src/products/categories/categories.controller.ts
+++ src/products/categories/categories.controller.ts
@@ -1,17 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
-import { get } from 'http';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Res, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CategoriesService } from './categories.service';
 import { Category } from './Category';
 
 @Controller('products/categories')
 export class CategoriesController {
-    @Get()
-    GetAll() {
-        return [{
-            id: 1,
-            color: "red",
-            icon: "home",
-            name: "some name"
-        } as Category
-        ];
+
+    constructor( private categoriesService: CategoriesService){}
+
+    @Get(":id")
+    GetById(@Param() params){
+        return this.categoriesService.getById(params.id);
+     
     }
+
+    @Get()
+    GetAll(){
+        return this.categoriesService.getAll();
+    }
+
+    @Patch(":id")
+    Update(@Param() params, @Body() item :Category){
+        return this.categoriesService.Update(params.id, item);
+    }
+
+    @Post()
+    Create(@Body()item:Category){
+        return this.categoriesService.Create(item);
+    }
+
+    @Delete(":id")
+    Delete(@Param() params){
+        return this.categoriesService.Delete(params.id);
+    }
+
+
 }
--- src/products/categories/categories.service.ts
+++ src/products/categories/categories.service.ts
@@ -16,9 +16,36 @@ export class CategoriesService {
 
   getById(id: number): Promise<Category> {
     return this.CategorysRepository.findOneBy({ id });
+  } 
+   
+  async Update(id: number, body: Category){
+    const oldItem = await this.getById(id);
+    if(!oldItem)
+    {
+      console.warn("The category does not exist!");
+      return false;
+    }
+    await this.CategorysRepository.save({id, ...body});
   }
 
-  async delete(id: number): Promise<void> {
+  async Create(body: Category){
+    const existingItem = await this.CategorysRepository.findOneBy({name: body.name});
+    if(existingItem)
+    {
+      console.warn("This category name already exists!");
+      return false;
+    }
+
+    await this.CategorysRepository.save(body);
+}
+
+  async Delete(id: number){
+    const item = await this.getById(id);
+    if(!item)
+    {
+      console.warn("The category does not exist!");
+      return false;
+    }
     await this.CategorysRepository.delete(id);
   }
 }
