65b5904d0f831080a70a2d677519a3fefcd42f38
Feride Elhennawy <40718869+FerideElhennawy@users.noreply.github.com>
2022-11-12 09:46:14 +0300

    Update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,9 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3307,
+      port: 3306,
       username: 'root',
-      password: 'darsh123',
+      password: 'password',
       database: 'PoS',
       entities: [Category],
       synchronize: true,
