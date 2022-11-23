f1f7c40ed729e82f45750e6ce64dec78d7a6fa72
Mustafa MG <mustafa.gamal@deeptecno.com>
2022-11-23 21:47:05 +0300

    fix username and password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccessControlModule } from 'nest-access-control';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { roles } from './auth/auth.roles';
 import { Category } from './products/categories/Category';
 import { Product } from './products/Product';
 import { ProductsModule } from './products/products.module';
@@ -14,16 +16,17 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
-      port: 3306,
+      port: 3307,
       username: 'root',
-      password: 'password',
+      password: 'darsh123',
       database: 'PoS',
       entities: [Category, Product, User, Shift],
       synchronize: true,
-    }),    
+    }),
+    AccessControlModule.forRoles(roles), 
     ProductsModule,    
     AuthModule,    
-    UsersModule,    
+    UsersModule,   
   ],
   controllers: [AppController],
   providers: [AppService],
